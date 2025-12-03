2728314410d75dfe1ffaa0bf32b512b70dd15c9b
Muhammad Hamza <hamza3256@live.co.uk>
2025-12-03 11:58:06 +0000

    feat: solve Day 01 Part 1

--- src/main/java/com/adventofcode/day01/Solution.java
+++ src/main/java/com/adventofcode/day01/Solution.java
@@ -16,32 +16,40 @@ public class Solution {
     }
     
     public static int solvePart1() throws IOException {
+        int count = 0;
         try (InputStream is = Solution.class.getClassLoader().getResourceAsStream(INPUT_FILE);
              BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-            
-            String line;
-            // TODO: Implement Part 1 solution
-            while ((line = br.readLine()) != null) {
+            String line = br.readLine();
+            int curr = 50;
+            while (line != null) {
                 line = line.trim();
-                // Process line here
+                char d = line.charAt(0);
+                boolean isLeft = d == 'L';
+                int num = Integer.parseInt(line.substring(1));
+                num = num % 100;
+                if (isLeft) {
+                    num *= -1;
+                }
+                curr += num;
+                if (curr > 99) {
+                    curr = curr - 100;
+                } else if (curr < 0) {
+                    curr = 100 + curr;
+                }
+
+                if (curr == 0) {
+                    count++;
+                }
+
+                line = br.readLine();
             }
         }
-        return 0;
+        return count;
     }
     
     public static int solvePart2() throws IOException {
-        try (InputStream is = Solution.class.getClassLoader().getResourceAsStream(INPUT_FILE);
-             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-            
-            String line;
-            // TODO: Implement Part 2 solution
-            while ((line = br.readLine()) != null) {
-                line = line.trim();
-                // Process line here
-            }
-        }
+        // TODO: Implement part 2 when ready
         return 0;
     }
 }
-
 
